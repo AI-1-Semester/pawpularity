e700f4c900742c6b1e31857bcd36e34ecfc89462
Bast38900 <bast38900@edu.ucl.dk>
2024-03-07 11:36:54 +0100

    huma_prediction

---
+++ human_prediction.py
@@ -0,0 +1,35 @@
+## Uge 10 - Opgave 2
+# Skriv en applikation, hvor man kan afprøve din model, 
+# Hvis der er et menneske på billedet du viser, skal billedet fjernes fra visningen.
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+train_data = pd.read_csv("./data/train/train.csv")
+
+##  Transform the data
+
+# Drop the columns not to be used
+correlated_data = train_data.drop(columns=['Id', 'Pawpularity', 'Action', 'Accessory', 'Near', 'Collage', 'Eyes', 'Face', 'Info', 'Subject Focus', 'Blur'], axis=1)
+
+df = pd.DataFrame(correlated_data)
+
+# Split the data into training and testing sets (test_size=0.2 means that 20% of the data will be used for testing)
+X = df.drop('Human', axis=1)
+y = df['Human']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a logistic regression model
+model = LogisticRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions
+predictions = model.predict(X_test)
+
+# Check the accuracy of the model
+accuracy = accuracy_score(y_test, predictions)
+print(f'Accuracy: {accuracy}')
