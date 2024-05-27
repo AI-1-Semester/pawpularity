79a64651426d6be76351d069f1fbe528b7b00294
Robert-California <evertonstyrer@gmail.com>
2024-05-27 14:02:08 +0200

    boosting working in UI

--- model_config.py
+++ model_config.py
@@ -1,4 +1,5 @@
-from pred_models.models import LinearRegressionModel, LogisticRegressionModel, NN_PawpularityModel, NN_HumanModel
+from pred_models.models import LinearRegressionModel, LogisticRegressionModel, NN_PawpularityModel, NN_HumanModel, AdaBoostModel
+
 
 
 
@@ -8,12 +9,13 @@ class ModelConfig:
         'linear_regression': LinearRegressionModel,
         'logistic_regression': LogisticRegressionModel,
         'nn_pawpularity': NN_PawpularityModel,
-        'nn_human': NN_HumanModel
+        'nn_human': NN_HumanModel,
+        'ada_boost': AdaBoostModel
     }
 
     # Maps use cases to the models that can be used for them
     use_case_models = {
-        'human_prediction': ['logistic_regression', 'nn_human'],
+        'human_prediction': ['logistic_regression','nn_human', 'ada_boost'],
         'pawpularity_score': ['linear_regression', 'nn_pawpularity'],
         'occlusion_detection': ['logistic_regression']
     }
@@ -36,6 +38,7 @@ class ModelConfig:
     model_class_to_name = {
         LinearRegressionModel: 'linear_regression',
         LogisticRegressionModel: 'logistic_regression',
+        AdaBoostModel: 'ada_boost',
         NN_PawpularityModel: 'nn_pawpularity',
         NN_HumanModel: 'nn_human'
     }
